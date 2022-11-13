639b419e602981885e6959e78844104b6e98297c
HITyfj <hityfj0720@gmail.com>
2022-11-13 14:53:08 +0800

    first commit

---
+++ scripts/main.js
@@ -0,0 +1,9 @@
+// let myHeading = document.querySelector('h1');
+// myHeading.textContent = 'hello world!';
+
+let iceCream = 'chocolate';
+if (iceCream === 'chocolate') {
+  console.error('123456');
+} else {
+  console.log('6789999');
+}
